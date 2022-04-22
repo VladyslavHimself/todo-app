fec6ba24b5172f750649aa7d91403d00ae57fd62
vladyslavhimself <vladyslavhimself@gmail.com>
2022-04-22 19:57:23 -0400

    added react-testing-library

---
+++ src/App/App.test.tsx
@@ -0,0 +1,5 @@
+import { describe, expect, it } from 'vitest';
+import App from './App';
+import { render, screen, userEvent } from '../../test-utils';
+
+describe('Test template', () => {});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
+    },
 });
