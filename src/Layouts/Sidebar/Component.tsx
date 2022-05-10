46836948efd66a91f407cca99a114feabf8f6418
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-09 22:22:29 -0400

    added sidebar layout

---
+++ src/Layouts/Sidebar/Component.tsx
@@ -0,0 +1,11 @@
+import classes from './styles.module.scss';
+
+function Sidebar(): JSX.Element {
+    return (
+        <div className={classes.sidebar}>
+            <h1>some sidebar</h1>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ src/Layouts/Sidebar/index.ts
@@ -0,0 +1,2 @@
+import Sidebar from './Component';
+export default Sidebar;
