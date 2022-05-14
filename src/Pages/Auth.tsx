8f0f47700f8f77c9e462a6d2d4c733933216ac34
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-13 20:43:49 -0400

    Done auth form

--- src/Pages/Auth.tsx
+++ src/Pages/Auth.tsx
@@ -1,11 +1,27 @@
+import Button from '../Components/Ui/Button';
+import Input from '../Components/Ui/Input';
+import ModalForm from '../Layouts/ModalForm';
 import classes from '../scss/auth.module.scss';
 
 function Auth() {
     return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Auth</h1>
-            </div>
+        <div className={classes.auth}>
+            <ModalForm className={classes.form}>
+                <h2>Login</h2>
+                <div className={classes.inputs}>
+                    <Input className={classes.input} placeholder="Email" />
+                    <Input className={classes.input} placeholder="Password" />
+                </div>
+
+                <div className={classes.buttons}>
+                    <Button className={classes.button} type="primary">
+                        Log In
+                    </Button>
+                    <Button className={classes.button} type="ghost">
+                        Register
+                    </Button>
+                </div>
+            </ModalForm>
         </div>
     );
 }
