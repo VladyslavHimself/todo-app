8868ce762c17a6f01e091b0ac32297351c534678
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-13 22:21:33 -0400

    Added CloseButton

--- src/Layouts/ModalForm/Component.tsx
+++ src/Layouts/ModalForm/Component.tsx
@@ -1,11 +1,32 @@
+import CloseButton from '../../Components/Ui/CloseButton';
 import classes from './styles.module.scss';
 
 interface IModalFormProps extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
+    withCloseButton?: boolean;
+    onCloseButtonClickHandler?: () => void;
 }
 
-export const ModalForm = ({ children, className }: IModalFormProps) => {
+export const ModalForm = ({
+    children,
+    className,
+    withCloseButton,
+    onCloseButtonClickHandler,
+}: IModalFormProps) => {
     const cls = [classes['modal-form'], className];
 
-    return <div className={cls.join(' ')}>{children}</div>;
+    return (
+        <div className={cls.join(' ')}>
+            {withCloseButton && (
+                <div className={classes['close-btn']}>
+                    <CloseButton onClick={onCloseButtonClickHandler} />
+                </div>
+            )}
+            {children}
+        </div>
+    );
+};
+
+ModalForm.defaultProps = {
+    withCloseButton: false,
 };
