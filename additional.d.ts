4f6363943c2ef5715efbb90783a5eda336c75d6e
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-18 11:54:03 -0400

    added firebase config and separate configs to .env file

--- additional.d.ts
+++ additional.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        NEXT_PUBLIC_FIREBASE_API_KEY: string;
-        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
-        NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
-        NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
-        NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
-        NEXT_PUBLIC_FIREBASE_APP_ID: string;
+        FIREBASE_API_KEY: string;
+        FIREBASE_AUTH_DOMAIN: string;
+        FIREBASE_PROJECT_ID: string;
+        FIREBASE_STORAGE_BUCKET: string;
+        FIREBASE_MESSAGING_SENDER_ID: string;
+        FIREBASE_APP_ID: string;
     }
 }
